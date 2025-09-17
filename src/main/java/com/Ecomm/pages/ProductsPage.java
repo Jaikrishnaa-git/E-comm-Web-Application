11d12bb1e4660a8d646de26c1e7bc7bd46f763f0
Jaikrishnaa <Jaikrishnaa.git@gmail.com>
2025-09-17 13:44:26 +0530

    update in testng file

--- src/main/java/com/Ecomm/pages/ProductsPage.java
+++ src/main/java/com/Ecomm/pages/ProductsPage.java
@@ -16,88 +16,81 @@ public class ProductsPage {
     private final By mensDropDown = By.xpath("//a[@href='#Men']");
     private final By kidsDropDown = By.xpath("//a[@href='#Kids']");
     private final By poloBrand = By.xpath("//a[@href='/brand_products/Polo']");
-    private final By HmBrand = By.xpath("//a[@href='/brand_products/H&M']");
-    private final By madameBrand = By.xpath("//a[@href='/brand_products/Madame']");
-    private final By mastBrand = By.xpath("//a[@href='/brand_products/Mast & Harbour']");
-    private final By babyHugBrand = By.xpath("//a[@href='/brand_products/Babyhug']");
-    private final By allenBrand = By.xpath("//a[@href='/brand_products/Allen Solly Junior']");
-    private final By kookieKidsBrand = By.xpath("//a[@href='/brand_products/Kookie Kids']");
-    private final By bibaBrand = By.xpath(" //a[@href='/brand_products/Biba']");
     private final By product1 = By.xpath("//a[@href='/product_details/1']");
     private final By quantity = By.xpath("//input[@id='quantity']");
     private final By addToCart = By.xpath("//a[@data-product-id='1']");
-    
+
     public ProductsPage(WebDriver driver) {
         this.driver = driver;
-        this.wait   = new WebDriverWait(driver, Duration.ofSeconds(15));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
-
 
     public void openProducts() {
         driver.get("https://www.automationexercise.com/products");
         wait.until(ExpectedConditions.visibilityOfElementLocated(firstProductAddBtn));
     }
-    
-    public void openProductpage()
-    {
-     	driver.findElement(productsLink).click();
+
+    public void openProductpage() {
+        wait.until(ExpectedConditions.elementToBeClickable(productsLink)).click();
     }
-  
+
     public void addFirstProductToCart() {
-        WebElement addBtn = wait.until(ExpectedConditions.elementToBeClickable(firstProductAddBtn));
-
-     
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", addBtn);
-
-       
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", addBtn);
-
-       
         try {
+            WebElement addBtn = wait.until(ExpectedConditions.elementToBeClickable(firstProductAddBtn));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", addBtn);
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", addBtn);
+
             WebElement continueBtn = wait.until(ExpectedConditions.elementToBeClickable(continueShoppingBtn));
             continueBtn.click();
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
-    
-    public boolean verifyDropDown()
-    {
-    	 return driver.findElement(womensDropDown).isDisplayed() && driver.findElement(mensDropDown).isDisplayed() && driver.findElement(kidsDropDown).isDisplayed();
+
+    public boolean verifyDropDown() {
+        return driver.findElement(womensDropDown).isDisplayed() &&
+               driver.findElement(mensDropDown).isDisplayed() &&
+               driver.findElement(kidsDropDown).isDisplayed();
     }
 
+    public boolean isBrandVisible() {
+        try {
+            return driver.findElement(poloBrand).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-	public boolean verifyBrands() {
-		
-		return false;
-	}
+    public void clickProduct() {
+        try {
+            WebElement product = wait.until(ExpectedConditions.presenceOfElementLocated(product1));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", product);
+            wait.until(ExpectedConditions.elementToBeClickable(product));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", product);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public boolean clickProduct2() throws InterruptedException {
+        try {
+            WebElement product = wait.until(ExpectedConditions.presenceOfElementLocated(product1));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", product);
+            wait.until(ExpectedConditions.elementToBeClickable(product));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", product);
+            Thread.sleep(2000);  // wait for quantity field to appear
+            return driver.findElement(quantity).isDisplayed();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
-	public boolean isBrandVisible() {
-		
-		
-		return driver.findElement(poloBrand).isDisplayed(); //&&
-//				driver.findElement(HmBrand).isDisplayed() &&
-//				driver.findElement(madameBrand).isDisplayed() &&
-//				driver.findElement(mastBrand).isDisplayed() &&
-//				driver.findElement(babyHugBrand).isDisplayed() && 
-//				driver.findElement(allenBrand).isDisplayed() &&
-//				driver.findElement(kookieKidsBrand).isDisplayed() &&
-//				driver.findElement(bibaBrand).isDisplayed();
-	}
-	public void clickProduct()
-	{
-		 driver.findElement(product1).click();
-	}
-	public boolean clickProduct2() throws InterruptedException
-	{
-		 driver.findElement(product1).click();
-		 Thread.sleep(2000);
-		return driver.findElement(quantity).isDisplayed();
-	}
-	public boolean cartVisible()
-	{
-		return driver.findElement(addToCart).isDisplayed();
-	}
-	
+    public boolean cartVisible() {
+        try {
+            return driver.findElement(addToCart).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
